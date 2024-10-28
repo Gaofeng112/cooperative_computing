e4e077a218e633830df13d5a779f45173d455c51
R_Gao <77912319+paradox-dx@users.noreply.github.com>
2024-10-28 17:21:24 +0800

    update code

--- Compiler/extract_onnx.py
+++
@@ -1,7 +0,0 @@
-import extract_onnx_lib
-# import torch
-# import onnx
-# import re
-print("python executed")
-extract_onnx_lib.split_onnx('npuCutInstruction.txt','npu')
-extract_onnx_lib.split_onnx('cpuCutInstruction.txt','cpu')
--- Compiler/run.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ../
-cmake --build . -- -j8
-cd ..
-./CoComputingCompiler --onnx=xxx.onnx
-python3 extract_onnx.py
-python3 rename_onnx.py
