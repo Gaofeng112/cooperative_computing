9bdea4a722655ba5984b7e6db82f7cd5ae4c5845
R_Gao <77912319+paradox-dx@users.noreply.github.com>
2024-11-19 16:53:08 +0800

    Update run.sh

--- New_Compiler/run.sh
+++ New_Compiler/run.sh
@@ -4,6 +4,7 @@ cd build
 cmake -G "Unix Makefiles" ../  #可加选项-DENABLE_TEST_OPTION
 cmake --build . -- -j8
 cd ..
+find . -type f -name "*.txt" ! -name "CMakeLists.txt" -exec rm {} +
 ./CoComputingCompiler --onnx=./net/vit_large_simplify.onnx
 #./CoComputingCompiler --onnx=./net/vision_model_simplify.onnx
 #./CoComputingCompiler --onnx=./net/generation_model_simplify.onnx
